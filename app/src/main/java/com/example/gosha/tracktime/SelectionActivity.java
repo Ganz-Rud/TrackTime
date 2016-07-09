bcc752d31210aed538de75c8ab09b66205e4bf37
Ganz <smirnov_ig@mail.ru>
2016-07-09 20:32:43 +0300

    Creare repository
    first commit

---
+++ app/src/main/java/com/example/gosha/tracktime/SelectionActivity.java
@@ -0,0 +1,31 @@
+package com.example.gosha.tracktime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    Button bus;
+    Button trolley;
+    Button tram;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+
+        bus = (Button)  findViewById(R.id.bus);
+        trolley = (Button) findViewById(R.id.trolley);
+        tram = (Button) findViewById(R.id.tram);
+
+        bus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
+
